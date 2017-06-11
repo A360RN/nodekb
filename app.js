e3b606f72041ada46a9c26af877e165ec9551ea9
A360RN <alonso.reynoso@unmsm.edu.pe>
2017-06-10 23:41:06 -0500

    Article Model, Mongoose and connection to MongoDB

--- app.js
+++ app.js
@@ -1,49 +1,53 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function () {
+    console.log('Connected to MongoDB');
+})
+
+// Check for DB errors
+db.on('error', function (err) {
+    console.log(err);
+});
 
 // Init App
 const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
 
 // Load view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Home route
-app.get('/', function(req, res){
-    let articles = [
-        {
-            id: 1,
-            title: 'Article One',
-            author: 'Alonso Reynoso',
-            body: 'This is article one'
-        },
-        {
-            id: 2,
-            title: 'Article Two',
-            author: 'Alonso Reynoso',
-            body: 'This is article two'
-        },
-        {
-            id: 3,
-            title: 'Article Three',
-            author: 'Alonso Reynoso',
-            body: 'This is article three'
+app.get('/', function (req, res) {
+    Article.find({}, function (err, articles) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
         }
-    ];
-    res.render('index', {
-        title: 'Articles',
-        articles: articles
-    });
+    })
+
 });
 
 // Add Route
-app.get('/articles/add', function(req, res){
+app.get('/articles/add', function (req, res) {
     res.render('add_article', {
         title: 'Add Article'
     })
 });
 
 // Start Server
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Server has started on port 3000...');
 });
