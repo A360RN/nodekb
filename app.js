7351c1b2a32875ea1709473207b38d4eccd2df06
A360RN <alonso.reynoso@unmsm.edu.pe>
2017-06-10 22:52:47 -0500

    Added Pug, mockup data to show

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
 const express = require('express');
+const path = require('path');
 
+// Init App
 const app = express();
 
+// Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home route
 app.get('/', function(req, res){
-    res.send('Hello World');
+    let articles = [
+        {
+            id: 1,
+            title: 'Article One',
+            author: 'Alonso Reynoso',
+            body: 'This is article one'
+        },
+        {
+            id: 2,
+            title: 'Article Two',
+            author: 'Alonso Reynoso',
+            body: 'This is article two'
+        },
+        {
+            id: 3,
+            title: 'Article Three',
+            author: 'Alonso Reynoso',
+            body: 'This is article three'
+        }
+    ];
+    res.render('index', {
+        title: 'Articles',
+        articles: articles
+    });
 });
 
+// Add Route
+app.get('/articles/add', function(req, res){
+    res.render('add_article', {
+        title: 'Add Article'
+    })
+});
+
+// Start Server
 app.listen(3000, function(){
     console.log('Server has started on port 3000...');
-})
+});
